8d8d2bbe1b082885be8f488d7f772dbf15b23a51
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2022-01-03 21:00:12 +0700

    app database index task-model

--- mevn-typescript/server/index.ts
+++ mevn-typescript/server/index.ts
@@ -1,6 +1,6 @@
-import express from 'express'
+import app from "./app";
+import { startConnection } from "./database";
 
-const app = express()
-
-app.listen(3000)
-console.log('server  is runing on  port 3000')
+startConnection();
+app.listen(3000);
+console.log("Server on port", 3000);
